0bb172f69dab9bcf281da09224fb3ce753ae64cd
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2017-03-07 12:49:33 +0100

    Fix warnings

--- lib/spejs/web/router.ex
+++ lib/spejs/web/router.ex
@@ -26,6 +26,6 @@ defmodule Spejs.Web.Router do
   # Other scopes may use custom stacks.
   scope "/api", Spejs.Web do
     pipe_through :api
-    post "interactions/devices", InteractionsController, :devices
+    post "/interactions/devices", InteractionsController, :devices
   end
 end
