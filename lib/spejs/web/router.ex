0c27b9767f55209030d901914bfcc315a78ea707
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2017-03-13 19:39:06 +0100

    Add user token to connection

--- lib/spejs/web/router.ex
+++ lib/spejs/web/router.ex
@@ -11,6 +11,7 @@ defmodule Spejs.Web.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug Coherence.Authentication.Session  # Add this
+    plug :put_user_token
   end
 
   pipeline :protected do
@@ -20,6 +21,7 @@ defmodule Spejs.Web.Router do
     plug :protect_from_forgery
     plug :put_secure_browser_headers
     plug Coherence.Authentication.Session, protected: true  # Add this
+    plug :put_user_token
   end
 
   pipeline :api do
@@ -60,5 +62,14 @@ defmodule Spejs.Web.Router do
     pipe_through :api
     post "/interactions/devices", InteractionsController, :devices
     get "/at_hackerspace", InteractionsController, :at_hackerspace
+  end
+
+  defp put_user_token(conn, _) do
+    case current_user = Coherence.current_user(conn) do
+      nil -> conn
+      _ ->
+        user_token = Phoenix.Token.sign(conn, "user_token", current_user.email)
+        conn |> assign(:user_token, user_token)
+    end
   end
 end
