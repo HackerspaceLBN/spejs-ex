8f37d9700990f97f1972464b7d44f6e806fcff98
Jacek Nakonieczny <jacek.nakonieczny@gmail.com>
2017-05-30 11:02:58 +0200

    Add Telecom API reference

--- lib/spejs/web/controllers/telecom_controller.ex
+++ lib/spejs/web/controllers/telecom_controller.ex
@@ -7,7 +7,23 @@ defmodule Spejs.Web.TelecomController do
 	def index(conn, %{"end_call" => _} = params), 
 		do: end_call(conn, params)
 	def index(conn, params), 
-		do: json(conn, Telecom.index(params))
+		do: json(conn, %{
+			"/start_call": %{
+				params: ["source", "destination"],
+				types: ["string", "string"],
+				required: :all
+			},
+			"/end_call": %{
+				params: ["source", "destination", "number"],
+				types: ["string", "string", "string"],
+				required: :one_of
+			},
+			"/": %{
+				params: ["start_call", "end_call"],
+				types: ["empty", "empty"],
+				required: :one_of
+			}
+		})
 
 	def start_call(conn, params), 
 		do: json(conn, Telecom.start_call(params))
